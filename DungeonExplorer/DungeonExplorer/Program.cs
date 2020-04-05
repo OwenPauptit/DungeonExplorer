64c965c7342d8849f3723b8f8a94bf0767d5d401
OwenPauptit <55768469+OwenPauptit@users.noreply.github.com>
2020-04-05 17:05:43 +0100

    Room generation created

---
+++ DungeonExplorer/DungeonExplorer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+
+    
+
+    class Program
+    {
+
+        public const int WINDOW_WIDTH = 60;
+        public const int WINDOW_HEIGHT = 20;
+
+
+        static void Main(string[] args)
+        {
+
+            Console.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+            while (true)
+            {
+                Console.Clear();
+                Room r = new Room();
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ DungeonExplorer/DungeonExplorer/Room.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    class Room
+    {
+
+        private readonly int _width, _height;
+        List<int[]> _doors;
+        List<string> _floorPlan;
+
+        static Random rnd = new Random();
+
+        public Room(int entranceX, int entranceY)
+        {
+            _doors = new List<int[]>();
+            _doors.Add(new int[] { entranceX, entranceY });
+            _width = rnd.Next(5,Program.WINDOW_WIDTH-3);
+            _height = rnd.Next(5, Program.WINDOW_HEIGHT-3);
+            generateDoors();
+        }
+
+        public Room()
+        {
+            _floorPlan = new List<string>();
+            _doors = new List<int[]>();
+            _width = rnd.Next(5, Program.WINDOW_WIDTH - 3);
+            _height = rnd.Next(5, Program.WINDOW_HEIGHT - 3);
+            generateDoors();
+            generateFloorPlan();
+            displayFloorPlan();
+        }
+
+        private void generateDoors()
+        {
+            int numDoors = (_doors.Count >0)? rnd.Next(_doors.Count + 1):rnd.Next(1, 4);
+            int x, y, wall;
+            bool valid;
+            while (_doors.Count < numDoors)
+            {
+                wall = rnd.Next(1, 5); // 1 = top, 2 = right, 3 = bottom, 4 = left
+                switch (wall)
+                {
+                    case 1:
+                        x = rnd.Next(1, _width - 1);
+                        y = 0;
+                        break;
+                    case 2:
+                        x = 0;
+                        y = rnd.Next(1, _height - 1);
+                        break;
+                    case 3:
+                        x = rnd.Next(1, _width - 1);
+                        y = _height-1;
+                        break;
+                    case 4:
+                        x = _width-1;
+                        y = rnd.Next(1, _height - 1);
+                        break;
+                    default:
+                        x = -1; // need for compiler
+                        y = -1;
+                        Console.WriteLine("Unexpected Random Outcome");
+                        Environment.Exit(-1);
+                        break;
+                }
+                valid = true;
+                foreach (var d in _doors)
+                {
+                    if (    (d[0] == x && (x == 0 || x == _width) )    ||    ( d[1] == y && (y == 0 || y == _height) )) // check if 2 doors are on the same wall - not allowed
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid)
+                {
+                    _doors.Add(new int[] { x, y });
+                }  
+            }
+            return;
+        }
+        private void generateFloorPlan()
+        {
+            string line, temp;
+
+            double calc = (Program.WINDOW_WIDTH - _width) / 2; // calculating spaces left, right and on top of the room
+            int spacesL = (int)Math.Floor(calc);
+            int spacesR = Program.WINDOW_WIDTH - spacesL;
+
+            calc = (Program.WINDOW_HEIGHT - _height) / 2;
+            int spacesT = (int)Math.Floor(calc);
+
+
+            for (int i = 0; i < this._height+ spacesT; ++i) // this used for clarity as could be confusion between Program.WINDOW_WIDTH / Program.WINDOW_HEIGHT and _width / _height
+            {
+
+
+                if (i < spacesT) {
+                    line = new string(' ', 1);
+                }
+                else if (i == spacesT || i == this._height +spacesT-1)
+                {
+                    line = new string(' ',spacesL) + new string('#', this._width) + new string(' ',spacesR);
+                }
+                else { 
+                    line = new string(' ', spacesL) + "#" + new string(' ', this._width-2) + "#" + new string(' ', spacesR);
+                }
+
+                foreach( var d in _doors)
+                {
+
+        
+
+                    if (d[1] + spacesT == i)
+                    {
+                        temp = "";
+                        for (int k = 0; k < line.Length; ++k)
+                        {
+                            if (k == d[0] + spacesL) 
+                            {
+                                temp += " ";
+                            }
+                            else
+                            {
+                                temp += line[k];
+                            }
+                        }
+                        line = temp;
+                    }
+                }
+
+
+                _floorPlan.Add(line);
+
+            }
+
+
+        }
+
+        public void displayFloorPlan()
+        {
+            foreach(var line in _floorPlan)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+
+    }
+}
